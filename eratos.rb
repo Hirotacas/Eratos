fde4b2d050ee32fe79a6869ccc9e27c83f55a79c
hirotaca <developer.hiro.east9.hey.world@gmail.com>
2016-08-04 19:34:50 +0900

    Ans.

--- eratos.rb
+++ eratos.rb
@@ -1,8 +1,16 @@
-require 'Prime'
-
 class Eratos
-  def self.prime(n)
-   p Prime.each(n.to_i).to_a.join(', ')
+  def self.prime(num)
+    search_list = (2..num).to_a
+    prime_list  = Array.new
+    delete_list = Array.new
+    loop {
+      prime = search_list.shift
+      break if prime.nil?
+      prime_list.push prime
+      search_list.map { |n| delete_list.push n if n % prime == 0 }
+      search_list -= delete_list
+    }
+    prime_list.join(', ')
   end
 end
 
